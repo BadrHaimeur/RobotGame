d1e85cefde7f22139db3b350be0125b54f163f1e
Badr Haimeur <badr@haimeur.com>
2021-11-06 15:08:47 +0100

    Add Room class that implements the IRoom interface

---
+++ RobotGame.Application/Room.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace RobotGame.Application
+{
+    /// <summary>
+    /// Gathers robots to do some activities.
+    /// 
+    /// <note type="note">
+    /// All commands will take effect only if there are no activities in the room.
+    /// </note>
+    /// </summary>
+    internal class Room : IRoom
+    {
+        protected List<IRobot> _robots;
+
+        public string Name { get; }
+        public bool IsBusy { get; protected set; }
+        public IReadOnlyCollection<IRobot> Robots { get => _robots.AsReadOnly(); }
+
+        public Room(string name)
+        {
+            this.Name = name;
+            this._robots = new List<IRobot>();
+        }
+
+        /// <summary>
+        /// Commands a bunch of robots to enter the current room.
+        /// </summary>
+        /// <param name="robots">The robots that must enter the room.</param>
+        public void GoIn(IEnumerable<IRobot> robots)
+        {
+            if (!IsBusy)
+            {
+                _robots.AddRange(robots);
+            }
+        }
+
+        /// <summary>
+        /// Commands one robot to enter the current room.
+        /// </summary>
+        /// <param name="robot">The robot that must enter the room.</param>
+        public void GoIn(IRobot robot)
+        {
+            if (!IsBusy)
+            {
+                _robots.Add(robot);
+            }
+        }
+
+        /// <summary>
+        /// Commands one robot or all robots to leave the current room.
+        /// </summary>
+        /// <param name="robot">
+        /// The robot that must leave the room. If it is null,
+        /// all robots will leave the room.
+        /// </param>
+        public void GoOut(IRobot robot = null)
+        {
+            if (!IsBusy)
+            {
+                if (robot != null)
+                {
+                    _robots.Remove(robot);
+                }
+                else
+                {
+                    _robots.Clear();
+                }
+
+            }
+        }
+    }
+}
