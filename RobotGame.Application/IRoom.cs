5d09b0000b62d5e8f8608225e318b0065801802c
Badr Haimeur <badr@haimeur.com>
2021-11-06 13:47:54 +0100

    Add the IRoom interface

---
+++ RobotGame.Application/IRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RobotGame.Application
+{
+    public interface IRoom
+    {
+        bool IsBusy { get; }
+        string Name { get; }
+        IReadOnlyCollection<IRobot> Robots { get; }
+
+        void GoIn(IEnumerable<IRobot> robots);
+        void GoIn(IRobot robot);
+        void GoOut(IRobot robot);
+    }
+}
