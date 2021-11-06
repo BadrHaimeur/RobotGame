3575cd1fde29867e31ce855c75cb301fea49bf94
Badr Haimeur <badr@haimeur.com>
2021-11-06 16:30:48 +0100

    Add ChargingRoom class that inherits from the Room class

---
+++ RobotGame.Application/ChargingRoom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RobotGame.Application
+{
+    /// <summary>
+    /// This is where robots get charged with electricity.
+    /// </summary>
+    public class ChargingRoom : Room, IChargeable
+    {
+        public event EventHandler DoneCharging;
+
+        public ChargingRoom(string name) : base(name)
+        {
+        }
+
+        /// <summary>
+        /// Fires the DoneCharging event.
+        /// </summary>
+        protected virtual void FireDoneCharging()
+        {
+            DoneCharging?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Charges all robots in the current room with electricity.
+        /// </summary>
+        public void Charge()
+        {
+            this.IsBusy = true;
+            foreach (var player in _robots)
+            {
+                if (player.BatteryLife < Robot.MaxBatteryLifeValue)
+                {
+                    player.Charge();
+                }
+            }
+
+            FireDoneCharging();
+            this.IsBusy = false;
+        }
+    }
+}
