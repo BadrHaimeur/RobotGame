a3d5326df10d39506f68d49c660a15ef5b2d44dc
Badr Haimeur <badr@haimeur.com>
2021-11-06 14:24:03 +0100

    Add dead battery life exception

---
+++ RobotGame.Application/Exceptions/DeadBatteryLifeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RobotGame.Application.Exceptions
+{
+    /// <summary>
+    /// Raises when the battery life of a robot is equal to zero.
+    /// </summary>
+    public class DeadBatteryLifeException : Exception
+    {
+        public DeadBatteryLifeException(string robotName) : base($"{robotName}'s battery is dead!! Please, charge it to play again.")
+        {
+        }
+    }
+}
