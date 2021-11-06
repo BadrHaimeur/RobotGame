8c7c8bccf0c979dcfee31839a4fc5be2164bfd14
Badr Haimeur <badr@haimeur.com>
2021-11-06 18:00:58 +0100

    Add RobotFactory class to create multiple instances of Robot

---
+++ RobotGame.Application/Helpers/RobotFactory.cs
@@ -0,0 +1,35 @@
+﻿using RobotGame.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace RobotGame.Application.Helpers
+{
+    public class RobotFactory
+    {
+        /// <summary>
+        /// Creates zero or more robot instances.
+        /// </summary>
+        /// 
+        /// <param name="numberOfRobots">
+        /// The number of instances to create. It will
+        /// create ten instances if not specified.
+        /// </param>
+        /// 
+        /// <returns>A list of robots</returns>
+        public static IList<IRobot> Create(uint numberOfRobots = 10)
+        {
+            var robots = new List<IRobot>();
+
+            if (numberOfRobots > 0)
+            {
+                for (int i = 1; i <= numberOfRobots; i++)
+                {
+                    var robotId = Guid.NewGuid().ToString().Split('-')[0];
+                    robots.Add(new Robot("Robot-" + robotId));
+                }
+            }
+            
+            return robots;
+        }
+    }
+}
