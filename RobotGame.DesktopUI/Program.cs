ddf7abb5630f4d9d6d27f958a9095200cca441bd
Badr Haimeur <badr@haimeur.com>
2021-11-06 18:49:09 +0100

    Fix form application naming conflicts

--- RobotGame.DesktopUI/Program.cs
+++ RobotGame.DesktopUI/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Forms = System.Windows.Forms;
 
 namespace RobotGame.DesktopUI
 {
@@ -14,9 +11,9 @@ namespace RobotGame.DesktopUI
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Forms.Application.EnableVisualStyles();
+            Forms.Application.SetCompatibleTextRenderingDefault(false);
+            Forms.Application.Run(new MainForm());
         }
     }
 }
